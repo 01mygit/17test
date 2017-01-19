f626e3afc41b1145970f18cac4cbfb1a8b4d78f7
FirstRespository <643515622@qq.com>
2017-01-19 10:21:13 +0800

    第一次提交

--- test/src/com/wk/Test.java
+++ test/src/com/wk/Test.java
@@ -3,6 +3,9 @@ package com.wk;
 public class Test {
 	public static void main(String[] args) {
 		System.out.print("-------------测试-------");
+		System.out.print("-------------测试02-------");
+		System.out.print("-------------测试03-------");
+
 	}
 
 }
